bd41603b57d707bf494734fc1cca517f8738259d
HanYinnn <47775118+HanYinnn@users.noreply.github.com>
2019-02-27 00:12:39 +0800

    Create q6_find_ascii_char.py

---
+++ p01/q6_find_ascii_char.py
@@ -0,0 +1,8 @@
+#Write a program q6_find_ascii_char.py that receives an ASCII code (an integer between 0 and 127) and displays its character.
+#For example, if the user enters 97, the program displays character a.
+ 
+#imput 
+code = int(input("Enter ASCII code: "))
+
+#compute data
+print(chr(code))
