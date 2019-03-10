8833554c29129e4694cf32524ff180ebd711c948
HanYinnn <47775118+HanYinnn@users.noreply.github.com>
2019-03-10 16:52:14 +0800

    Create  q07_miles_to_kilometres.py

---
+++ p02/q07_miles_to_kilometres.py
@@ -0,0 +1,11 @@
+#Write a program that displays the following two tables side-by-side (note that 1 mile is 1.609 kilometres):
+
+#Miles	Kilometers	Kilometres	Miles
+#1    	1.609     	20        	12.430
+#2    	3.218     	25        	15.538
+
+#...
+#9    	14.481    	60        	37.290
+#10   	16.090    	65        	40.398
+
+
