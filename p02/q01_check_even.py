727b9fb810dd59a2380e953b87455850a2566c84
HanYinnn <47775118+HanYinnn@users.noreply.github.com>
2019-03-10 14:43:14 +0800

    Create  q01_check_even.py

---
+++ p02/q01_check_even.py
@@ -0,0 +1,10 @@
+#Write a program that reads an integer and checks whether it is even. 
+
+number = int(input("Enter a number: "))
+
+if number == 2:
+    print("Even!")
+elif number % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
