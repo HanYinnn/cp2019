c27e9d7cb328c5a278ceae0f5b0386b68732cb57
HanYinnn <47775118+HanYinnn@users.noreply.github.com>
2019-03-24 16:23:59 +0800

    Update q07_miles_to_kilometres.py

--- p02/q07_miles_to_kilometres.py
+++ p02/q07_miles_to_kilometres.py
@@ -8,4 +8,6 @@
 #9    	14.481    	60        	37.290
 #10   	16.090    	65        	40.398
 
-
+print("Miles""\t""Kilometers""\t\t""Kilometers""\t""Miles")
+for i in range(1,11):
+    print(i, " "*(6-(len(str(i)))), (i*1.609)," "*(22-len(str(i*1.609))),(i*5+20), " "*(14-len(str(i*5+20))),"{0:.3f}".format((i*5+20)/1.609 ))
