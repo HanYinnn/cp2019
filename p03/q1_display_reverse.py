3d883c31dd4fee02a50fc693c4363ef3967e608b
HanYinnn <47775118+HanYinnn@users.noreply.github.com>
2019-03-24 16:15:46 +0800

    Create q1_display_reverse.py

---
+++ p03/q1_display_reverse.py
@@ -0,0 +1,15 @@
+#Write a function reverse_int(n) to display an integer in reverse order:
+#For example, reverse_int(3456) displays 6543.
+-------------------------------------------------------------------------------
+def reverse():
+    x = input("Enter integer with at least 2 digits: ")
+    print(x[::-1])
+
+reverse()
+
+----------------------------------OR-------------------------------------------
+
+x = input("Enter integer with at least 2 digits: ")
+x[::-1]
+
+-------------------------------------------------------------------------------
